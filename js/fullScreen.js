5671965897c02b4c84781b99abce62a0a02aa748
Azuls1 <156047024+Azuls1@users.noreply.github.com>
2024-09-30 11:19:38 -0500

    Create fullScreen.js

---
+++ js/fullScreen.js
@@ -0,0 +1,14 @@
+const iframe = document.getElementById('rblxiframe');
+const expandIcon = document.getElementById('expandIcon');
+
+expandIcon.addEventListener('click', function() {
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) {
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) {
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) {
+        iframe.msRequestFullscreen();
+    }
+});
